c9d854cca8681f65be76fa65884c5567806cdc81
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-28 00:05:12 -0400

    implement delete

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Modal = (props) => {
-
   // Instead of being a direct child of the parent component , its renders into the div of modal
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        Dfasdfdsafasfbkasdjfjadshflskf
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      {
+        // Prevent event from bubbling up}
+      }
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Modal from '../../Modal';
+import history from '../../history';
 
+// React Fragment -> Invisible element that has no impact on DOM
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
-      StreamDelete <Modal />
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are your sure you want to delete this stream"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
     </div>
   );
 };
