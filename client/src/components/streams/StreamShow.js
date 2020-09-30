f67c78b20808d1d2d60930d21974d259cf42cec9
Kevin Reyes <reyeskevin9767@gmail.com>
2020-09-29 23:42:18 -0400

    add video player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+
+    // First Time player is rendered
+    this.buildPlayer();
+  }
+
+  // When page refreshes
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  // Build Video Player
+  buildPlayer() {
+    const { id } = this.props.match.params;
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    // Create Video Player
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    // Pass video element to player
+    this.player.attachMediaElement(this.videoRef.current);
+
+    // Load video
+    this.player.load();
   }
 
   render() {
@@ -13,7 +54,9 @@ class StreamShow extends React.Component {
     }
     const { title, description } = this.props.stream;
     return (
+      // Video Player
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h3>{description}</h3>
       </div>
